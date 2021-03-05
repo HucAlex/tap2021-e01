e7d4a19091f76a27d0eafab5edafaea3946e3a04
HucAlex <alex.huc.fl@gmail.com>
2021-03-05 07:58:06 +0200

    First modification

---
+++ Test1_HucAlexandru/Test1_HucAlexandru/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test1_HucAlexandru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
